924166375c2b8c97c3deff6d1ebf36a2e62a2f72
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-18 09:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  //Write your code here
+  const newRow = sampleTable.insertRow(0);
+  const newRowCell1 = newRow.insertCell(0);
+  newRowCell1.innerHTML = "New Cell1"
+  const newRowCell2 = newRow.insertCell(1);
+  newRowCell2.innerHTML = "New Cell2"
+
 }
+
+let sampleTable = document.getElementById("sampleTable");
+
+
